601b9b3a1d8207e40247b15556c90b4e2d9ae776
KeepingRunning <1599949878@qq.com>
2025-12-18 00:38:02 +0800

    add smtp for email verification

--- util/config.go
+++ util/config.go
@@ -19,6 +19,9 @@ type Config struct {
 	TokenSymmetricKey    string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
 	AccessTokenDuration  time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
 	RefreshTokenDuration time.Duration `mapstructure:"REFRESH_TOKEN_DURATION"`
+	EmailSendName        string        `mapstructure:"EMAIL_SEND_NAME"`
+	EmailSendAddress     string        `mapstructure:"EMAIL_SEND_ADDRESS"`
+	EmailSendPassword    string        `mapstructure:"EMAIL_SEND_PASSWORD"`
 }
 
 // LoadConfig reads configuration from file or environment variable
